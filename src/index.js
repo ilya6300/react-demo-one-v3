ecb5656addc883aabce5efb2c4df873bf8fd684c
IlyaAztek <ilya63001990@mail.ru>
2023-03-25 13:18:34 +0300

    git commit -m "commit version 5"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js?4';
+import App from './App.js?5';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
